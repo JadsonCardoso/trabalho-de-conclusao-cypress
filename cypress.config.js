37b273ba3ce1bed04168057ce515b59fe1a98de9
JadsonCardoso <jadsonc190@gmail.com>
2025-10-31 12:04:53 -0300

    RELATORIO

--- cypress.config.js
+++ cypress.config.js
@@ -2,18 +2,15 @@ const { defineConfig } = require("cypress");
 require('dotenv').config()
 
 module.exports = defineConfig({
+   retries: {
+      openMode: 0,
+      runMode: 1
+    },
   e2e: {
     env: {
       BASE_URL_PADRAO: process.env.BASE_URL_PADRAO
     },
     reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-      reportDir: "cypress/reports",
-      overwrite: false,
-      html: true,
-      json: true,
-      charts: true,
-    },
     screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
